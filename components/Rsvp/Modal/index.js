fa7c4fb53ea6bc00a09b2fa310473c34ecab6d8c
jamesgrubb <22652+jamesgrubb@users.noreply.github.com>
2021-10-31 14:57:34 +0000

    fixed minor errors

--- components/Rsvp/Modal/index.js
+++ components/Rsvp/Modal/index.js
@@ -1,5 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+export default function Modal({ show, children }) {
+	const [isBrowser, setIsBrowser] = useState(false);
+	useEffect(() => {
+		setIsBrowser(true);
+	}, []);
 
-export default function Modal() {
+	if (isBrowser) {
+		return ReactDOM.createPortal(
+			<div>
+				Hello from createPortal
+				<div className=''>{children}</div>
+			</div>
+		);
+	}
+
 	return <div></div>;
 }
--- components/Rsvp/index.js
+++ components/Rsvp/index.js
@@ -1,6 +1,6 @@
 import { useForm } from 'react-hook-form';
 
-const Rsvp = (props,children) => {
+const Rsvp = (props, children) => {
 	const {
 		register,
 		handleSubmit,
@@ -9,16 +9,11 @@ const Rsvp = (props,children) => {
 	const onSubmit = (data) => console.log(data);
 	console.log(errors);
 
-	const
-
 	return (
 		<div className='fixed bottom-0 left-0 z-10 flex items-center justify-center w-full h-16 space-x-4 text-center bg-red-800 sm:h-20'>
 			<strong className='font-normal text-teal-300 font-lora'>
 				{' '}
-				<button
-					onClick={handleClick}
-					type='button'
-					className='btn'>
+				<button type='button' className='btn'>
 					RSVP
 				</button>
 			</strong>
