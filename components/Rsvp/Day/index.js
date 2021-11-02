e312f0109a9ba261df7950a0c46a6805dff6a3ad
jamesgrubb <22652+jamesgrubb@users.noreply.github.com>
2021-11-02 02:32:18 +0000

    checkboxes

--- components/Rsvp/Day/index.js
+++ components/Rsvp/Day/index.js
@@ -46,6 +46,9 @@ export default function Day(props) {
 	const youngDiner = data.filter((item) => {
 		return item.fields.Type === 'YoungDiner';
 	});
+	const dietItems = data.filter((item) => {
+		return item.fields.Type === 'Diet';
+	});
 	return (
 		<>
 			<div className='z-10 w-full transform translate-x-0'>
@@ -55,95 +58,117 @@ export default function Day(props) {
 				</h3>
 				<form
 					onSubmit={handleSubmit(onSubmit)}
-					className='z-10 flex-col flex-wrap justify-start block h-full px-2 py-8 space-y-8 font-mono transform translate-x-0 bg-red-100 sm:flex sm:flex-col sm:p-8 sm:items-start sm:px-8 sm:py-8'>
-					<h3 className='tracking-widest text-center uppercase'>
-						Starters
-					</h3>
-					{starters.map((item) => {
-						return (
-							<label
-								key={item.id}
-								htmlFor={item.id}
-								className='inline-flex items-center flex-1 gap-3 space-x-2 text-left font-lora'>
-								<input
-									className='text-teal-600 form-radio'
-									{...register(item.fields.Type)}
-									type='radio'
-									name={item.fields.Type}
-									id={item.id}
-									value={item.fields.Nickname}
-								/>
-								{item.fields.Dish}
-							</label>
-						);
-					})}
-					{errors.Starter ? (
-						<strong className='flex-1 w-full font-mono text-sm font-light text-right text-red-400 peer'>
-							Dont fancy anything {props.name}?
-						</strong>
-					) : (
-						''
-					)}
-					<h3 className='tracking-widest text-center uppercase'>
-						Mains
-					</h3>
-					{mains.map((item) => {
-						return (
-							<label
-								className='inline-flex items-center gap-3 font-lora'
-								key={item.id}
-								htmlFor={item.id}>
-								<input
-									className='text-teal-600 form-radio'
-									type='radio'
-									{...register(item.fields.Type)}
-									name={item.fields.Type}
-									id={item.id}
-									value={item.fields.Nickname}
-								/>
-								{item.fields.Dish}
-							</label>
-						);
-					})}
-					{errors.Main ? (
-						<strong className='w-full font-mono text-sm font-light text-right text-red-400 felx-1 peer'>
-							Dont fancy anything {props.name}?
-						</strong>
-					) : (
-						''
-					)}
-					<h3 className='tracking-widest text-center uppercase'>
-						Young Diner Menu
-					</h3>
-					<label
-						className='inline-flex items-center gap-3 font-lora'
-						key={youngDiner[0].id}
-						htmlFor={youngDiner[0].id}>
-						<input
-							className='text-teal-600 form-radio'
-							type='radio'
-							{...register('Main')}
-							name={'Main'}
-							id={youngDiner[0].id}
-							value={youngDiner[0].fields.Nickname}
-						/>
-						{youngDiner[0].fields.Dish}
-					</label>
+					className='z-10 flex-wrap justify-between h-full px-2 py-8 transform translate-x-0 bg-red-100 sm:flex sm:p-8 sm:px-8 sm:py-8'>
+					<div className='flex-col justify-start block w-full space-y-3 prose-sm sm:space-y-5 sm:prose sm:items-start sm:w-3/5 '>
+						<h3 className='tracking-widest text-center uppercase sm:text-left'>
+							Starters
+						</h3>
+						{starters.map((item) => {
+							return (
+								<label
+									key={item.id}
+									htmlFor={item.id}
+									className='inline-flex items-center flex-1 gap-3 space-x-2 text-left font-lora'>
+									<input
+										className='text-teal-600 form-radio'
+										{...register(item.fields.Type)}
+										type='radio'
+										name={item.fields.Type}
+										id={item.id}
+										value={item.fields.Nickname}
+									/>
+									{item.fields.Dish}
+								</label>
+							);
+						})}
+						{errors.Starter ? (
+							<strong className='flex-1 w-full font-mono text-sm font-light text-right text-red-400 peer'>
+								Dont fancy anything {props.name}?
+							</strong>
+						) : (
+							''
+						)}
+						<h3 className='tracking-widest text-center uppercase sm:text-left'>
+							Mains
+						</h3>
+						{mains.map((item) => {
+							return (
+								<label
+									className='inline-flex items-center gap-3 font-lora'
+									key={item.id}
+									htmlFor={item.id}>
+									<input
+										className='text-teal-600 form-radio'
+										type='radio'
+										{...register(item.fields.Type)}
+										name={item.fields.Type}
+										id={item.id}
+										value={item.fields.Nickname}
+									/>
+									{item.fields.Dish}
+								</label>
+							);
+						})}
+						{errors.Main ? (
+							<strong className='w-full font-mono text-sm font-light text-right text-red-400 felx-1 peer'>
+								Dont fancy anything {props.name}?
+							</strong>
+						) : (
+							''
+						)}
+						<h3 className='tracking-widest text-center uppercase sm:text-left'>
+							Young Diner Menu
+						</h3>
+						<label
+							className='inline-flex items-center gap-3 font-lora'
+							key={youngDiner[0].id}
+							htmlFor={youngDiner[0].id}>
+							<input
+								className='text-teal-600 form-radio'
+								type='radio'
+								{...register('Main')}
+								name={'Main'}
+								id={youngDiner[0].id}
+								value={youngDiner[0].fields.Nickname}
+							/>
+							{youngDiner[0].fields.Dish}
+						</label>
 
-					{errors.Main ? (
-						<strong className='w-full font-mono text-sm font-light text-right text-red-400 felx-1 peer'>
-							Dont fancy anything {props.name}?
-						</strong>
-					) : (
-						''
-					)}
-					<h3 className='tracking-widest text-center uppercase'>
-						Desert
-					</h3>
-					<p className='pl-7 font-lora' key={desert[0].id}>
-						{desert[0].fields.Dish}
-					</p>
-					<div className='flex w-full space-x-2 sm:justify-start'>
+						{errors.Main ? (
+							<strong className='w-full font-mono text-sm font-light text-right text-red-400 felx-1 peer'>
+								Dont fancy anything {props.name}?
+							</strong>
+						) : (
+							''
+						)}
+						<h3 className='tracking-widest text-center uppercase sm:text-left'>
+							Desert
+						</h3>
+						<p className='pl-7 font-lora' key={desert[0].id}>
+							{desert[0].fields.Dish}
+						</p>
+					</div>
+					<div className='flex flex-col self-start w-full font-light prose-sm sm:w-1/5'>
+						{dietItems.map((item) => {
+							return (
+								<label
+									key={item.id}
+									htmlFor={item.id}
+									className='inline-flex items-baseline flex-1 gap-3 space-x-2 font-mono text-left'>
+									<input
+										className='text-teal-600 form-checkbox'
+										{...register(item.fields.Type)}
+										type='checkbox'
+										name={item.fields.Type}
+										id={item.id}
+										value={item.fields.Name}
+									/>
+									{item.fields.Name}
+								</label>
+							);
+						})}
+					</div>
+					<div className='flex w-full mt-4 space-x-2 sm:justify-start'>
 						<button className='btn' type='submit'>
 							<span>Yes Chef!</span>
 							<FiMail />
--- pages/api/food.js
+++ pages/api/food.js
@@ -1,13 +1,17 @@
+import { get } from 'react-hook-form';
 import { table, getMinifiedRecords } from '../api/utils/Airtable';
 export default async function handler(req, res) {
 	try {
+		const dietRecords = await table('Intolerances').select({}).firstPage();
+		const minifiedDietRecords = getMinifiedRecords(dietRecords);
 		const records = await table('Menu')
 			.select({ view: 'Grid view' })
 			.firstPage();
 		const minifiedRecords = getMinifiedRecords(records);
 		res.statusCode = 200;
 		console.log(minifiedRecords);
-		await res.json(minifiedRecords);
+
+		await res.json([...minifiedRecords, ...minifiedDietRecords]);
 	} catch (error) {
 		console.error(error);
 		res.json({ msg: 'cant get food, hungry' });
